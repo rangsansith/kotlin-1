79cda476d0a3d3191298731e4c28c9a3a4e02d34
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2018-06-25 00:38:17 +0300

    Support varargs of inline class types with non-trivial spread

--- compiler/testData/codegen/box/unsignedTypes/varargsOfUnsignedTypes.kt
+++ compiler/testData/codegen/box/unsignedTypes/varargsOfUnsignedTypes.kt
@@ -15,11 +15,17 @@ fun box(): String {
     val uints = uint(1u, 2u, 3u)
     if (sum(*uints) != 6u) return "Fail 1"
 
+    val complextUInts = uint(4u, *uints, 5u, *uints, 6u)
+    if (sum(*complextUInts) != 27u) return "Fail 2"
+
     val nullableUInts = nullableUInt(1u, null, 2u, null)
-    if (sum(*nullableUInts) != 3u) return "Fail 2"
+    if (sum(*nullableUInts) != 3u) return "Fail 3"
 
     val inlinedUInts = inlinedUInt(1u, 3u)
-    if (sum(*inlinedUInts) != 4u) return "Fail 3"
+    if (sum(*inlinedUInts) != 4u) return "Fail 4"
+
+    val complexInlinedUInts = inlinedUInt(*inlinedUInts, 3u, *inlinedUInts)
+    if (sum(*complexInlinedUInts) != 11u) return "Fail 5"
 
     return "OK"
 }
